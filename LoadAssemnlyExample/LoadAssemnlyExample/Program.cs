2ea66f9254052fc98ab8bf36b61ee52c037e7d93
ESSARRAJ <essarraj.fouad@gmail.com>
2018-01-14 12:29:03 +0100

    init commit

---
+++ LoadAssemnlyExample/LoadAssemnlyExample/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoadAssemnlyExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Load assemnly
+            Assembly assembly = Assembly.LoadFrom("ProjectManager.Core.dll");
+            var ReferencedAssemblies = assembly.GetReferencedAssemblies();
+
+            // we can  Chek existance of all referenced assemblie
+            foreach (var item in ReferencedAssemblies)
+            {
+                // Assembly assembly = Assembly.LoadFrom(item.Name);
+            }
+
+            // Show All Types 
+            var teypes = assembly.GetTypes();
+            foreach (var type in teypes)
+            {
+                Console.WriteLine(type.Name);
+
+                // Show Properties
+                foreach (var item in type.GetProperties())
+                {
+                    Console.WriteLine("\t" + item.Name);
+                }
+
+            }
+            Console.Read();
+
+        }
+    }
+}
